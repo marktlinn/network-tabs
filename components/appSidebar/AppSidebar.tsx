32b2f7005450d37c90e58fe62fd71bc27a85cb5f
marktlinn <marktlinn@gmail.com>
2025-09-28 20:30:24 +0100

    add: collapsible component + add sidebar elements

--- components/appSidebar/AppSidebar.tsx
+++ components/appSidebar/AppSidebar.tsx
@@ -1,9 +1,21 @@
-import { Home, Settings } from 'lucide-react'
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from '@/components/ui/sidebar'
+import { ChevronDown, Home, Plus, Settings, Workflow, LifeBuoy, MessageSquareQuote } from 'lucide-react'
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupAction,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarSeparator
+} from '@/components/ui/sidebar'
 import Link from 'next/link'
 import { useTheme } from 'next-themes'
 import AppSidebarHeader from '@/components/appSidebar/AppSidebarHeader'
 import AppSidebarFooter from '@/components/appSidebar/AppSidebarFooter'
+import { CollapsibleTrigger, Collapsible, CollapsibleContent } from '@/components/ui/collapsible'
 
 const sidebarItems = [
 	{
@@ -18,6 +30,11 @@ const sidebarItems = [
 	}
 ]
 
+const applications = [
+	{ name: "Node" },
+	{ name: "Office" }
+]
+
 const AppSidebar = () => {
 	return (
 		<Sidebar collapsible='icon' >
@@ -25,7 +42,7 @@ const AppSidebar = () => {
 
 			<SidebarContent>
 				<SidebarGroup>
-					<SidebarGroupLabel>Application</SidebarGroupLabel>
+					<SidebarGroupLabel>Admin</SidebarGroupLabel>
 					<SidebarGroupContent>
 						<SidebarMenu>
 							{
@@ -43,6 +60,62 @@ const AppSidebar = () => {
 						</SidebarMenu>
 					</SidebarGroupContent>
 				</SidebarGroup>
+
+
+				<SidebarGroup>
+					<SidebarGroupLabel>Application</SidebarGroupLabel>
+					<SidebarGroupAction>
+						<Plus className="cursor-pointer" /> <span className="sr-only">Add Project</span>
+					</SidebarGroupAction>
+
+					<SidebarMenu>
+						{
+							applications.map((app) => (
+								<SidebarMenuItem key={app.name}>
+									<SidebarMenuButton asChild>
+										<Link href="#">
+											<Workflow />
+											<span>{app.name}</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>)
+							)
+						}
+					</SidebarMenu>
+				</SidebarGroup>
+
+
+				<Collapsible defaultOpen className="group/collapsible">
+					<SidebarGroup>
+						<SidebarGroupLabel asChild>
+							<CollapsibleTrigger>
+								Help
+								<ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+							</CollapsibleTrigger>
+						</SidebarGroupLabel>
+						<CollapsibleContent>
+							<SidebarGroupContent />
+
+							<SidebarMenu>
+								<SidebarMenuItem>
+									<SidebarMenuButton asChild>
+										<Link href="#">
+											<LifeBuoy />
+											<span>Help</span>
+										</Link>
+									</SidebarMenuButton>
+									<SidebarMenuButton asChild>
+										<Link href="#">
+											<MessageSquareQuote />
+											<span>Feedback</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							</SidebarMenu>
+						</CollapsibleContent>
+					</SidebarGroup>
+				</Collapsible>
+
 			</SidebarContent>
 
 			<AppSidebarFooter />
